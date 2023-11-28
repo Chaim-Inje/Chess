fd443660deef525eb86ab62d5bace3c5c60d8595
chaim.ingi <chaim.ingi@mail.huji.ac.il>
2023-11-28 21:08:24 +0200

    Update files

--- board.py
+++ board.py
@@ -1,20 +1,38 @@
 from typing import *
-import
+
 
 class Board:
     def __init__(self):
-        self.board: list[list[Optional[pieces.Pieses]]] = [[None for i in range(8)] for j in range(8)]
+        self.board = [[None for i in range(8)] for j in range(8)]
         self.white_pieces = []
         self.black_pieces = []
 
     def square_content(self, square):
         return self.board[square[0]][square[1]]
 
-    def make_move(self, square_src, square_dest):
-        if self.board[square_src[0]][square_src[1]] is None:
-            return False
+    def if_blocked(self, square_1, square_2):
+        if square_1[0] == square_2[0]:
+            low_col, high_col = min(square_1[1], square_2[1]) + 1, max(square_1[1], square_2[1])
+            while low_col < high_col:
+                if self.board[square_2[0]][low_col]:
+                    return True
+                low_col += 1
+        elif square_1[1] == square_2[1]:
+            low_row, high_row = min(square_1[0], square_2[0]) + 1, max(square_1[0], square_2[0])
+            while low_row < high_row:
+                if self.board[low_row][square_2[1]]:
+                    return True
+                low_row += 1
         else:
-            move_type = (self.board[square_src[0]][square_src[1]]).move_type(square_src, square_dest)
-            if not move_type:
-                return False
+            low_point, high_point = min(square_1, square_2, key=lambda square: square[0]), max(square_1, square_2, key=lambda square: square[0])
+            while low_point[0] < high_point[0]:
+                if low_point[1] < high_point[1]:
+                    low_point[1] += 1
+                else:
+                    low_point[1] -= 1
+                low_point[0] += 1
+                if self.board[low_point[0]][low_point[1]]:
+                    return True
+        return False
+
 
