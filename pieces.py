24e2da45d60b00a692b4853b54b61dd46f0f963a
yakov_1234 <yankiwolman@gmail.com>
2023-11-28 19:03:55 +0200

    basic

--- pieces.py
+++ pieces.py
@@ -1,14 +1,16 @@
 import typing
 import board
 class Pieces:
-    def __init__(self, name: str, color: str):
+    def __init__(self, name: str, color: str, board: board.Board):
         self.name = name, self.color = color
+        self.board = board
 
     def color(self) -> str:
         return self.color()
 
     def king_moves(self, sqr: tuple):
-        pass
+        moves = []
+        for i in range(sqr[0]-1, )
 
     def rook_moves(self, sqr: tuple):
         pass
