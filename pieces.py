05d1ac404b05f8aff4a65091d723bbe79a6f0725
yakov_1234 <yankiwolman@gmail.com>
2023-12-18 17:37:03 +0200

    basic

--- pieces.py
+++ pieces.py
@@ -75,19 +75,19 @@ class Pieces:
     funcs = {"king": king_moves, "queen": queen_moves, "rook": rook_moves,
              "bishop": bishop_moves, "knight": knight_moves}
     def __init__(self, name: str, color: bool):
-        self.name = name
-        self.color = color
+        self.__name = name
+        self.__color = color
 
     def color(self) -> bool:
-        return self.color
+        return self.__color
 
     def name(self) -> str:
-        return self.name
+        return self.__name
 
     def possible_moves(self, sqr: list):
-        return pawn_moves(sqr, self.color) if self.name == "pawn" else\
-            self.funcs[self.name](sqr)
+        return pawn_moves(sqr, self.__color) if self.__name == "pawn" else\
+            self.funcs[self.__name](sqr)
 
     def possible_eats(self, sqr: list):
-        return pawn_eats(sqr, self.color) if self.name == "pawn" else\
-            self.funcs[self.name](sqr)
+        return pawn_eats(sqr, self.__color) if self.__name == "pawn" else\
+            self.funcs[self.__name](sqr)
