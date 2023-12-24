b1e88988b6e9282b50b2783e93bedf9d23bcae15
chaim.ingi <chaim.ingi@mail.huji.ac.il>
2023-12-24 20:26:30 +0200

    Update files

--- game.py
+++ game.py
@@ -7,10 +7,13 @@ from stockfish import Stockfish
 from time import sleep
 white = (255, 255, 255)
 black = (120, 65, 0)
-  # Add this line to define the color red
 yellow = (255, 200, 0)
 darker_yellow = (200, 150,0)
 square_size = 70
+LEFT_BAR = 0
+UP_BAR = 0
+RIGHT_BAR = 0
+DOWN_BAR = 0
 
 class Game:
     def __init__(self, surface, two_players = False):
@@ -60,7 +63,6 @@ class Game:
                 self.castling.remove([7,0])
             if [7,7] in self.castling:
                 self.castling.remove([7,7])
-        piece = self.board[src]
         if self.board[src].name() == "king" and src[1] == dst[1]-2:
             self.draw_movement([src[0], 5], [src[0], 7])
             self.board.move_piece([src[0], 7], [src[0], 5])
@@ -69,7 +71,7 @@ class Game:
             self.board.move_piece([src[0], 0], [src[0], 3])
         self.board.move_piece(src, dst)
         promotion_piece_for_stockfish = ''
-        if self.board[dst].name() == 'pawn' and (dst[0]== 0 or dst[0] ==7):
+        if self.board[dst].name() == 'pawn' and (dst[0] == 0 or dst[0] ==7):
             my_boy = self.promotion(self.board[dst].color())
             self.board.delete_piece(dst)
             self.board.insert_piece(my_boy,dst)
@@ -94,8 +96,8 @@ class Game:
         self.board.delete_piece(src)
         for i in range(90):
             self.surface.blit(pygame.image.load(piece.path_to_image()), (
-            (src[1] + 0.26) * square_size + phase_col * i,
-            (7-src[0] + 0.26) * square_size + phase_row * i))
+            (src[1] + 0.26) * square_size + phase_col * i+LEFT_BAR,
+            (7-src[0] + 0.26) * square_size + phase_row * i+UP_BAR))
             pygame.display.update()
             self.draw_board()
         self.board.insert_piece(piece, src)
@@ -172,18 +174,18 @@ class Game:
                     color = white
                 else:
                     color = black
-                pygame.draw.rect(self.surface, color, (col * square_size, (7-row) * square_size, square_size, square_size))
+                pygame.draw.rect(self.surface, color, (col * square_size+LEFT_BAR, (7-row) * square_size+UP_BAR, square_size, square_size))
                 if [row, col] in list_of_squares:
                     if (row + col) % 2 == 1:
                         for i in range(15):
-                            pygame.draw.rect(self.surface, (255, 200+4*i-1, 18*i), pygame.Rect(col * square_size + i, (7-row) * square_size + i, square_size - 2*i, square_size - 2*i), 1)
+                            pygame.draw.rect(self.surface, (255, 200+4*i-1, 18*i), pygame.Rect(col * square_size + i+LEFT_BAR, (7-row) * square_size + i+UP_BAR, square_size - 2*i, square_size - 2*i), 1)
                     else:
                         for i in range(15):
-                            pygame.draw.rect(self.surface, (255-10*i, 200-10*i, 0), pygame.Rect(col * square_size + i, (7-row) * square_size + i, square_size - 2*i, square_size - 2*i), 1)
+                            pygame.draw.rect(self.surface, (255-10*i, 200-10*i, 0), pygame.Rect(col * square_size + i+LEFT_BAR, (7-row) * square_size + i+UP_BAR, square_size - 2*i, square_size - 2*i),1)
                 # Draw the pieces
                 piece = self.board[[row, col]]
                 if piece is not None:
-                    self.surface.blit(pygame.image.load(piece.path_to_image()), ((col + 0.26) * square_size, (7-row + 0.26) * square_size))
+                    self.surface.blit(pygame.image.load(piece.path_to_image()), ((col + 0.26) * square_size+LEFT_BAR, (7-row + 0.26) * square_size+UP_BAR))
 
     def stalemate(self, color:bool) -> bool:
         for piece in (self.board.white_pieces() if color else self.board.black_pieces()):
@@ -212,11 +214,11 @@ class Game:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             # Calculate the row and column of the clicked square
-            down = [7-(pos[1] // square_size), pos[0] // square_size]
+            down = [7-((pos[1]-UP_BAR) // square_size), (pos[0]-LEFT_BAR) // square_size]
         if pygame.mouse.get_focused():
             pos = pygame.mouse.get_pos()
             # Calculate the row and column of the clicked square
-            hovered = [7-(pos[1] // square_size), pos[0] // square_size]
+            hovered = [7-((pos[1]-UP_BAR) // square_size), (pos[0]-LEFT_BAR) // square_size]
         return down, hovered
 
     def game_manager(self):
@@ -260,7 +262,7 @@ def main():
     pygame.event.set_blocked(None)
     pygame.event.set_allowed(pygame.MOUSEBUTTONDOWN)
     pygame.event.set_allowed(pygame.QUIT)
-    board_size = (square_size * 8, square_size * 8)
+    board_size = (square_size * 8+ RIGHT_BAR+LEFT_BAR, square_size * 8+UP_BAR+DOWN_BAR)
     display_surface = pygame.display.set_mode(board_size)
     game = Game(display_surface,False)
     game.draw_board()
